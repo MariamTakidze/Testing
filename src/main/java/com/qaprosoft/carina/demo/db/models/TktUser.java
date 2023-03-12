20a42dd6ef0db9b8222e2f87dbebe69e9886383e
MTakidze <marefango18@gmail.com>
2023-03-12 13:04:49 -0700

    add tkt user

---
+++ src/main/java/com/qaprosoft/carina/demo/db/models/TktUser.java
@@ -0,0 +1,14 @@
+package com.qaprosoft.carina.demo.db.models;
+
+public class TktUser  {
+    private final String email = "maregango18@gmail.com";
+    private final String password = "MareeGang27";
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
